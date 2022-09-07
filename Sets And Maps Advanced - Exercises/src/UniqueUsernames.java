3b0444b3c50830ed8c831efb91bbd9a8328eb326
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-09-07 22:27:14 +0100

    Add files via upload

---
+++ Sets And Maps Advanced - Exercises/src/UniqueUsernames.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> usernames = new LinkedHashSet<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+
+            String username = scanner.nextLine();
+            usernames.add(username);
+        }
+
+        for (String username : usernames) {
+            System.out.println(username);
+        }
+    }
+}
